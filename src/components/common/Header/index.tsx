87d2fb70bd55cd4668ea83bd0d590a45cc4a7a64
inji0212 <in17100170@kakao.com>
2024-09-09 20:48:53 +0900

    style : switch 토글 스타일

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -4,9 +4,9 @@ import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
-    <header className=" container flex h-16 justify-between  items-center border-b border-gray-200 bg-header">
+    <header className="  w-full flex h-[70px] justify-between  items-center border-b border-gray-200 dark:border-gray-800 bg-header px-[11vw] ">
       <Link href={"/"}>Inji's</Link>
-      <div className="flex flex-row items-center space-x-1">
+      <div className="flex flex-row items-center space-x-2">
         <Link href={"/contact"}>Contact</Link>
         <a href="https://github.com/inji0212">
           <FaGithub />
--- src/components/common/Header/mode-toggle/index.tsx
+++ src/components/common/Header/mode-toggle/index.tsx
@@ -4,15 +4,58 @@ import { useTheme } from "next-themes";
 
 export default function ModeToggle() {
   const { systemTheme, theme, setTheme } = useTheme();
+
+  // 현재 테마를 가져옵니다.
   const currentTheme = theme === "system" ? systemTheme : theme;
-  const handleToggle = () =>
+  const isChecked = currentTheme === "light"; // 체크 상태를 다크 모드인지 여부로 결정합니다.
+
+  // 토글 스위치 상태를 변경하는 함수
+  const handleChange = () => {
     setTheme(currentTheme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <switch
-      onClick={handleToggle}
-      className="  text-black dark:text-purple-400 flex items-center transition duration-300 "
+    <div
+      className={`inline-flex items-center ${isChecked ? "opacity-100" : ""}`}
     >
-      {theme === "dark" ? <Sun size={16} /> : <Moon size={16} />}
-    </switch>
+      {/* 스위치 body */}
+      <div
+        onClick={handleChange}
+        className={`relative flex items-center justify-start w-14 h-[30px] rounded-full transition-colors duration-300 ease-in-out ${
+          isChecked ? "bg-fuchsia-100" : "bg-gray-400"
+        } cursor-pointer`}
+      >
+        {/* 스위치 toggle */}
+        <span
+          onClick={handleChange}
+          className={`absolute rounded-full transform transition-transform duration-300 ease-in-out cursor-pointer shadow ${
+            isChecked
+              ? "translate-x-[28px] bg-white"
+              : "translate-x-[4px] bg-gray-800"
+          } w-[24px] h-[24px] flex items-center justify-center`}
+        >
+          {/* 토글 상태에 따라 아이콘 변경 */}
+          {isChecked ? (
+            <Sun
+              size={18}
+              className="text-fuchsia-600 transition-all duration-300 ease-in-out p-0 "
+            /> // 다크 모드 아이콘
+          ) : (
+            <Moon
+              size={18}
+              className="text-yellow-400 transition-all duration-300 ease-in-out"
+            /> // 라이트 모드 아이콘
+          )}
+        </span>
+      </div>
+
+      {/* 체크박스 숨김 */}
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleChange}
+        className="sr-only"
+      />
+    </div>
   );
 }
