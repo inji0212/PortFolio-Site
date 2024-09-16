82758d00ac52adb8293dd6ef23af704b6e5f2b58
inji0212 <in17100170@kakao.com>
2024-09-16 15:15:33 +0900

    fix :Header

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,8 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // 경로 별칭 설정: @를 src 폴더로 매핑
     config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
-
-    // 파일 확장자 추가: .ts와 .tsx 파일도 인식하도록 설정
     config.resolve.extensions.push(".ts", ".tsx");
-
     return config;
   },
 };
